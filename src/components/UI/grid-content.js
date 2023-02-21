a179dc24634b2ddb18be32a71a6e1f05083f0de8
Sam Fazakerley <sam@cornellbox.studio>
2023-02-21 10:47:33 +0000

    add dark/light content modes for different sections

--- src/components/UI/grid-content.js
+++ src/components/UI/grid-content.js
@@ -1,30 +1,37 @@
 import React, { useEffect, useRef } from 'react'
 
 //Pass in the layout e.g(--center-4, etc)
-const GirdContent = ({ children, layout }) => {
-  // Here we add the SVG brand shape vertically running down column-start: 3
+const GridContent = ({ children, layout, background, mode }) => {
   // We get the height of the content object
   const content = useRef(null)
   const hourglass = useRef(null)
   // We need a pagination object on right
 
+  //Set background and colour scheme of content
+
+  // Here we add the SVG brand shape vertically running down column-start: 3
   useEffect(() => {
     // We need svg brand mark on left
     const contentBounds = content.current.getBoundingClientRect()
-
+    //Get the parent relative position
+    const parentBounds = content.current.parentElement.getBoundingClientRect()
     window.requestAnimationFrame(() => {
-      hourglass.current.style.top = `${contentBounds.top}px`
+      //The actual top is subtracted from the parent top
+      const relativeTop = contentBounds.top - parentBounds.top
+      hourglass.current.style.top = `${relativeTop}px`
       hourglass.current.style.height = `${contentBounds.height}px`
     })
   }, [content, hourglass])
   return (
-    <div className="grid-column-12">
-      <div className="hourglass" ref={hourglass}></div>
-      <div className={`grid-content ${layout}`} ref={content}>
-        {children}
+    <div className={`container ${mode}`}>
+      <div className="grid-column-12">
+        <div className="hourglass" ref={hourglass}></div>
+        <div className={`grid-content ${layout}`} ref={content}>
+          {children}
+        </div>
       </div>
     </div>
   )
 }
 
-export default GirdContent
+export default GridContent
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,13 @@
 import { GatsbySeo } from 'gatsby-plugin-next-seo'
 import React from 'react'
 import Layout from '../components/Layout'
-import GirdContent from '../components/UI/grid-content'
+import GridContent from '../components/UI/grid-content'
 
 const Home = () => (
   <Layout>
     <GatsbySeo title="Home Page" />
-    <GirdContent layout=" --center-4">
-      <h1>Hi There!</h1>
+    <GridContent layout=" --center-4" mode="dark-mode" background="grey">
+      <h4>Introduction</h4>
       <p>
         <br />
         <br />
@@ -34,10 +34,38 @@ const Home = () => (
         imperdiet nisl feugiat id. Curabitur sagittis lectus augue, vel
         dignissim lacus molestie vel. Aenean gravida hendrerit lacus vitae
         porttitor. Nullam varius lorem sapien. Donec sed tortor turpis.
-        <br />
-        <br />
       </p>
-    </GirdContent>
+    </GridContent>
+    <GridContent layout=" --center-4" mode="light-mode" background="image">
+      <h4>Startegy</h4>
+      <p>
+        <br />
+        <br />
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam eu
+        gravida velit. Donec scelerisque imperdiet fringilla. Aliquam sagittis
+        vel sapien vitae mattis. Vestibulum sodales dolor neque, quis feugiat
+        odio volutpat sit amet. Quisque tristique tristique nibh. Integer
+        euismod iaculis turpis, sed finibus nisl mattis eu. Phasellus mattis
+        luctus purus et ultrices. Donec in nisl nec turpis accumsan sagittis nec
+        id justo. Morbi ultrices, elit sit amet posuere maximus, nisl sem
+        laoreet nibh, non gravida nunc velit nec erat. Fusce nunc odio, lobortis
+        ac elit ac, gravida dignissim nunc. Nunc accumsan vehicula felis, eu
+        auctor risus ultricies vel. Nulla vulputate condimentum viverra.
+        <br />
+        <br />
+        Maecenas ut massa urna. Maecenas eget odio in mauris luctus sollicitudin
+        ut eget mauris. Curabitur semper quis lectus sed egestas. Quisque quis
+        velit aliquet, accumsan metus ut, congue felis. Mauris vitae urna id
+        magna tempus laoreet. Integer sit amet condimentum orci, quis hendrerit
+        enim. In eu metus erat. Morbi eleifend viverra purus sit amet interdum.
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum
+        accumsan porttitor nibh, ut interdum augue suscipit nec. Aenean ac orci
+        id quam elementum pellentesque. Cras volutpat tristique ante, a
+        imperdiet nisl feugiat id. Curabitur sagittis lectus augue, vel
+        dignissim lacus molestie vel. Aenean gravida hendrerit lacus vitae
+        porttitor. Nullam varius lorem sapien. Donec sed tortor turpis.
+      </p>
+    </GridContent>
   </Layout>
 )
 
